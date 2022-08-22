8ddd3265919eda65992bad05c335554d724d0b9d
SquireTrelony <wowk.iv@gmail.com>
2022-08-22 11:53:59 +0300

    Class view

--- news/models.py
+++ news/models.py
@@ -10,7 +10,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
 
 
     class Meta:
@@ -24,7 +24,7 @@ class News(models.Model):
 
 
     def get_absolute_url(self):
-        return reverse('view_news', kwargs={"news_id": self.pk})
+        return reverse('view_news', kwargs={"pk": self.pk})
 
 
 
